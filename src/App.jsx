13ec401424e91310e4d15aecce9b049a1c38ec18
YumbaUsesGit <yumba.work846@gmail..com>
2026-02-19 07:36:34 +0200

    fixed the naming issue and added hover states

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar.jsx';
-import Footer from './Footer.jsx';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Skills from './pages/Skills.jsx';
-import Projects from './pages/Projects.jsx';
-import Contact from './pages/Contact.jsx';
+  import React from 'react';
+  import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+  import Navbar from './Navbar.jsx';
+  import Footer from './Footer.jsx';
+  import Home from './pages/Home.jsx';
+  import About from './pages/About.jsx';
+  import Skills from './pages/Skills.jsx';
+  import Projects from './pages/Projects.jsx';
+  import Contact from './pages/Contact.jsx';
 
-function App() {
-  return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+  function App() {
+    return (
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/skills" element={<Skills />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 
-export default App;
+  export default App;
